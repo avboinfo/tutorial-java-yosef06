1e503b7f650600d318fd929d3a12f416f2b60a42
Sandro Gallo <sandro.gallo@careware.it>
2024-02-01 00:51:53 +0100

    start

---
+++ A_Basic/ProveFormattazioni.java
@@ -0,0 +1,23 @@
+/*
+ * Prova Formattazioni
+ */
+
+package provaformattazioni;
+
+/**
+ *
+ * @author sandro.gallo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        for (float f=8; f<12; f+=0.5) {
+            System.out.printf("%6.2f\n", f);
+        }
+        
+    }
+}
