59c5eb6397e3a453d3d4ed63f2beee515af09b10
Sandro Gallo <5992750+sandrogallo@users.noreply.github.com>
2024-02-08 12:03:17 +0000

    update

---
+++ S_Stack/Main.java
@@ -0,0 +1,15 @@
+/*
+** Esempio di uso di uno Stack (Pila)
+** Sandro Gallo - 08/02/2024
+*/
+
+package S_Stack;
+
+public class Main {
+
+    public static void main( String[] args ) {
+        Stack<String> s = new Stack<>();
+        System.out.println(s);
+    }
+
+}
